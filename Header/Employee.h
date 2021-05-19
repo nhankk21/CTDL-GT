4096270a4ec1a3ff59c7ca292d2c2aac40a4971e
quanglinhta96 <71243233+quanglinhta96@users.noreply.github.com>
2021-05-19 14:29:10 +0700

    05192021

--- Header/Employee.h
+++ Header/Employee.h
@@ -2,13 +2,29 @@
 #include <string.h>
 
 class Employee{
-	private:
-		string codeEmp;
-		string lastName;
-		string firstName;
-		string middleName;
-		bool sex;// 0: male, 1: female
-		Bill *toBill;	
+	public:
+	string codeEmp;
+	string lastName;
+	string firstName;
+	string middleName;
+	bool sex;// 0: male, 1: female
+	Bill *toBill;
+	
+	Employee(string codeEmp, string lastName, string middleName, string firstName, bool sex) {
+		// Tao 1 employee moi
+		this->codeEmp = codeEmp;
+		this->lastName = lastName;
+		this->middleName = middleName;
+		this->firstName = firstName;
+		this->sex = sex;
+		toBill = NULL;
+	}
+	void update(string lastName, string middleName, string firstName, bool sex) {
+		this->lastName = lastName;
+		this->middleName = middleName;
+		this->firstName = firstName;
+		this->sex = sex;
+	}	
 };
 
 class listEmployee{
